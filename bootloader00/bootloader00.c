af838bb85a692762b21101403fc2acedf8d279b9
Oleksandr Motsak <http://goo.gl/mcpzY>
2013-05-08 00:19:04 +0200

    Mostly UART1 example

--- bootloader00/bootloader00.c
+++ bootloader00/bootloader00.c
@@ -186,6 +186,44 @@ int notmain ( void )
     unsigned int data;
 */
 
+
+    PUT32(AUX_ENABLES,1);
+    PUT32(AUX_MU_IER_REG,0);
+    PUT32(AUX_MU_CNTL_REG,0);
+    PUT32(AUX_MU_LCR_REG,3);
+    PUT32(AUX_MU_MCR_REG,0);
+    PUT32(AUX_MU_IER_REG,0);
+    PUT32(AUX_MU_IIR_REG,0xC6);
+    PUT32(AUX_MU_BAUD_REG,270);
+
+    ra=GET32(GPFSEL1);
+    ra&=~(7<<12); //gpio14
+    ra|=2<<12;    //alt5
+    PUT32(GPFSEL1,ra);
+
+    PUT32(GPPUD,0);
+    for(ra=0;ra<150;ra++) dummy(ra);
+    PUT32(GPPUDCLK0,(1<<14));
+    for(ra=0;ra<150;ra++) dummy(ra);
+    PUT32(GPPUDCLK0,0);
+
+    PUT32(AUX_MU_CNTL_REG,2);
+
+    ra=0;
+        hexstring(0x12345678);
+
+    while(1)
+    {
+        while(1)
+        {
+            if(GET32(AUX_MU_LSR_REG)&0x20) break;
+        }
+        PUT32(AUX_MU_IO_REG,0x30+(ra++&7));
+    }
+
+return (0);
+
+
     PUT32(AUX_ENABLES,1);
     PUT32(AUX_MU_IER_REG,0);
     PUT32(AUX_MU_CNTL_REG,0);
@@ -214,10 +252,17 @@ int notmain ( void )
     uart_newline();
     uart_write("Hell Or WORD!:: ");
 
+
+
+
+
+
     while(1)
     {
         ra = uart_recv ();
-	uart_send ( ra );
+        
+        hexstring( ra ); 
+//	uart_send ( ra );
 	
 	if( ra == 'Q' )
 	  break;
